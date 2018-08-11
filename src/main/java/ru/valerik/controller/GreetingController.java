55f9c0e6290af386eb3f214ae89e2476fbbd566e
valerik <valeraberdov@yandex.ru>
2018-08-11 13:48:02 +0700

    from mustache to ftl

--- src/main/java/ru/valerik/controller/GreetingController.java
+++ src/main/java/ru/valerik/controller/GreetingController.java
@@ -3,14 +3,13 @@ package ru.valerik.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.valerik.model.Message;
 import ru.valerik.model.User;
 import ru.valerik.repository.MessageRepository;
-
-import java.util.Map;
 
 @Controller
 public class GreetingController {
@@ -19,15 +18,21 @@ public class GreetingController {
     private MessageRepository messageRepository;
 
     @GetMapping("/")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Map<String, Object> model) {
-        model.put("name", name);
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
         return "greeting";
     }
 
     @GetMapping("/main")
-    public String main(Map<String, Object> model) {
-        Iterable<Message> messages = messageRepository.findAll();
-        model.put("messages", messages);
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
+        Iterable<Message> messages;
+        if (null != filter && !filter.isEmpty()) {
+            messages = messageRepository.findByTag(filter);
+        } else {
+            messages = messageRepository.findAll();
+        }
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
         return "main";
     }
 
@@ -36,25 +41,12 @@ public class GreetingController {
             @AuthenticationPrincipal User user,
             @RequestParam String text,
             @RequestParam String tag,
-            Map<String, Object> model) {
+            Model model) {
         Message message = new Message(text, tag, user);
         messageRepository.save(message);
 
         Iterable<Message> messages = messageRepository.findAll();
-        model.put("messages", messages);
-        return "main";
-    }
-
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter, Map<String, Object> model) {
-        Iterable<Message> messages;
-        if (null != filter && !filter.isEmpty()) {
-            messages = messageRepository.findByTag(filter);
-        } else {
-            messages = messageRepository.findAll();
-        }
-
-        model.put("messages", messages);
+        model.addAttribute("messages", messages);
         return "main";
     }
 }
--- src/main/java/ru/valerik/controller/RegistrationController.java
+++ src/main/java/ru/valerik/controller/RegistrationController.java
@@ -2,6 +2,7 @@ package ru.valerik.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.valerik.model.Role;
@@ -9,7 +10,6 @@ import ru.valerik.model.User;
 import ru.valerik.repository.UserRepository;
 
 import java.util.Collections;
-import java.util.Map;
 
 @Controller
 public class RegistrationController {
@@ -17,15 +17,16 @@ public class RegistrationController {
     UserRepository userRepository;
 
     @GetMapping("/registration")
-    public String registration() {
+    public String registration(Model model) {
+        model.addAttribute("message", "");
         return "registration";
     }
 
     @PostMapping("/registration")
-    public String addUser(User user, Map<String, Object> model) {
+    public String addUser(User user, Model model) {
         User userFromDb = userRepository.findByUsername(user.getUsername());
         if (null != userFromDb) {
-            model.put("message", "User exists!!!");
+            model.addAttribute("message", "User exists!!!");
             return "registration";
         }
         user.setActive(true);
